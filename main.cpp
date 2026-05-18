3adb196f97288bf9dd7a029b3cbe806b0cf20384
Christine Susic <csusic022@insite.4cd.edu>
2026-05-18 15:36:23 -0700

    Milestone 2

--- main.cpp
+++ main.cpp
@@ -10,35 +10,39 @@ using namespace std;
 
 int main() {
     ifstream fin("data.txt"); //open file
-    int count = 0; //initialize count to zero
+    int max = 0; //initialize count and max count to zero
     string origin, dest; //string to read from file
     
     std::map<std::string, int> flights;
     
-    //cout << "Busiest airport(s) with count:" << count << endl;
     //cout << "Airports with traffic in range [" << "]:" << endl;
     //cout << "Airports with traffic in range [" << "]:" << endl;
     
     //read codes from file
     while (fin >> origin) {
         fin >> dest;
-        flights.insert({origin, count});
-        flights.insert({dest, count});
-        count++;
+        flights[origin]++; 
+        flights[dest]++;
     }
     
-    int max = 0;
+    //All airport traffic
     cout << "All airport traffic counts:" << endl;
     for (std::map<std::string, int>::iterator it = flights.begin(); it != flights.end(); it++) { 
-        //it->first (origin), it->second (dest)
+        //output it->first (origin), it->second (dest)
         cout << it->first << " " << it->second << endl; 
         //find max count
         if (it->second > max) {
             max = it->second;
-        
         }
     }
     
+    //Busiest airport traffic
+    cout << "\nBusiest airport(s) with count: " << max << endl;
+    for (std::map<std::string, int>::iterator it = flights.begin(); it != flights.end(); it++) {
+        if (it->second == max) {
+            cout << it->first << " " << it-> second;
+        }
+    }
     
     fin.close(); //close file
 
